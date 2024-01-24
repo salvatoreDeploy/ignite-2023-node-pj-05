5b8cb7ae2fc890e1d1e6362e6408e68f06a57b52
salvatoreDeploy <liderhenrique@gmail.com>
2024-01-24 14:13:53 -0300

    List of most recent questions and pagination

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { envSchema } from './env';
 import { AuthenticateModule } from './authenticate/auth.module';
 import { AutneticateSessionController } from './controller/auntenticate-session.controller';
 import { CreateQuestionController } from './controller/create-question.controller';
+import { FetchRecentQuestionController } from './controller/fetch-recent-question.controller';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { CreateQuestionController } from './controller/create-question.controlle
     }),
     AuthenticateModule
   ],
-  controllers: [CreateAccountController, AutneticateSessionController, CreateQuestionController],
+  controllers: [CreateAccountController, AutneticateSessionController, CreateQuestionController, FetchRecentQuestionController],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controller/fetch-recent-question.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { JwtAuthGaurd } from "src/authenticate/jwt-auth-guard";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type PageQueryParam = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGaurd)
+export class FetchRecentQuestionController{
+
+  constructor(private prisma: PrismaService){}
+
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParam) {
+
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page -1) * perPage,
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+
+    return {
+      questions
+    }
+  }
+}
